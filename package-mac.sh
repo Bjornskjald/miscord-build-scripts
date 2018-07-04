7bac886b96d08236bef56455f0840bfdfbb8b657
Bjornskjald <github@bjorn.ml>
2018-07-04 21:53:29 +0200

    fixed macos zip name

--- package-mac.sh
+++ package-mac.sh
@@ -7,7 +7,7 @@ pushd $BUILD_DIR
 sed s/%version-here%/$VERSION/g Miscord.app/Contents/Info.plist > Miscord.app/Contents/Info.plist
 mv miscord-macos Miscord.app/Contents/Resources/script
 
-zip -r miscord-$1-macos.zip Miscord.app
+zip -r miscord-macos.zip Miscord.app
 rm -r Miscord.app
 
 popd
